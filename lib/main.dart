627c269e5200d3679787e3b41b11e304e1a9454b
suryabhansingh <its.v.surya9@gmail.com>
2021-09-02 12:00:59 +0530

    Day1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hear_phone_store/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-          child:
-              Center(child: Container(child: Text('Flutter Demo Home Page')))),
+      home: HomePage(),
     );
   }
 }
