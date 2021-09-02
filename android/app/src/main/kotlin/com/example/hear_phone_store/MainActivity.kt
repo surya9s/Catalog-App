a1d53f043c3530686a2f9cd1654d70c15d339814
suryabhansingh <its.v.surya9@gmail.com>
2021-09-02 11:36:10 +0530

    First Commit

---
+++ android/app/src/main/kotlin/com/example/hear_phone_store/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hear_phone_store
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+          child:
+              Center(child: Container(child: Text('Flutter Demo Home Page')))),
+    );
+  }
+}
