bea136798dd0f0041ad7243675d185767cd5cd79
suryabhansingh <its.v.surya9@gmail.com>
2021-09-06 01:53:54 +0530

    day9 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -34,20 +34,54 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     var scaffold = Scaffold(
+      backgroundColor: Colors.grey[300],
       appBar: AppBar(
         title: Text('Phone store'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ItemWedget(
-                    item: CatalogModel.items[index],
+        child: (CatalogModel.items.isNotEmpty)
+            ? GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 8,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    child: GridTile(
+                      child: Image.network(item.image),
+                      header: Container(
+                        child: Text(
+                          item.name,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(color: Colors.indigo),
+                      ),
+                      footer: Container(
+                        child: Text((item.price.toString()),
+                            style: TextStyle(color: Colors.white)),
+                        padding: EdgeInsets.all(12),
+                        decoration: BoxDecoration(color: Colors.indigo),
+                      ),
+                    ),
                   );
                 },
+                itemCount: CatalogModel.items.length,
               )
+            //  ListView.builder(
+            //     itemCount: CatalogModel.items.length,
+            //     itemBuilder: (BuildContext context, int index) {
+            //       return ItemWedget(
+            //         item: CatalogModel.items[index],
+            //       );
+            //     },
+            //   )
             : Center(
                 child: CircularProgressIndicator(),
               ),
